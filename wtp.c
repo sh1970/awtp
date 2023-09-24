20802fc2236140ef933489eee256d6dac60f0e29
liguojiang <guojiang@591wifi.com>
2023-09-24 11:06:42 +0000

    fix reload bug

--- wtp.c
+++ wtp.c
@@ -201,8 +201,10 @@ void save_ac_ip_to_ucentral(char *ip)
 		/*
 		system("/etc/init.d/ucentral restart");   
 		*/
-		//system("reload_config");
+#if 0
+		system("reload_config");
 		sleep(DISCOVERY_INTERVAL);
+#endif
 		system("/usr/bin/killall -9 flock");
 		system("/sbin/reboot");
 	}
